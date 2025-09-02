bf2e80f989bbed75b3af6fed984bef185449cd80
diego69-foda <diego.bkuhn@gmail.com>
2025-09-02 11:36:50 -0300

    com  importações de css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import './Header.css';
 
 export default function Header() {
   return (
